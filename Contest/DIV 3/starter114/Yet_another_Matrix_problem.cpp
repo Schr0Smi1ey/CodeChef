6898974f8fd0b3d7c7a4a5258f6ddd07b9054ee4
Sarafat Karim <128882721+Schrosmiley@users.noreply.github.com>
2023-12-28 00:23:43 +0600

    27-12-23

--- Contest/DIV 3/starter114/Yet_another_Matrix_problem.cpp
+++ Contest/DIV 3/starter114/Yet_another_Matrix_problem.cpp
@@ -37,35 +37,6 @@ void solution(){
     else{
         swap(arr[0][2],arr[1][1]);
     }
-
-    
-
-    // int num = 1;
-    // for(int i=0;i<n;i++){
-    //     arr[0][i] = num;
-    //     num += 2;
-    // }
-    // for(int i=1;i<n;i++){
-    //     arr[i][0] = num;
-    //     num += 2;
-    // }
-    // int x = (((n*n)/2) + 1 - n - (n - 1))/ (n-1);
-    // for(int j = (n-x);j < n;j++){
-    //     for(int i = 1 ;i<n;i++){
-    //         if(num > (n*n)) break;
-    //         arr[j][i] = num;
-    //         num += 2;
-    //     }
-    // }
-    // num = 2;
-    // for(int i = 0 ;i<n-1;i++){
-    //     for(int j = 0;j<n;j++){
-    //         if(arr[i][j] == 0){
-    //             arr[i][j] = num;
-    //             num += 2;
-    //         }
-    //     }
-    // }
     for(int i = 0 ;i<n;i++){
         for(int j = 0;j<n;j++){
             cout << arr[i][j] << " ";
